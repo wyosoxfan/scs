b3fe19812bb4c88d2ba6c17b725984eda1131197
Neil Christensen <neil@Neils-MacBook-Pro.local>
2020-04-03 23:21:01 -0600

    map

--- js/function.js
+++ js/function.js
@@ -3,10 +3,9 @@
 // Code By Webdevtrick ( https://webdevtrick.com )
 const items = document.querySelectorAll(".accordion a");
 
-function toggleAccordion(){
+function toggleAccordion() {
   this.classList.toggle('active');
   this.nextElementSibling.classList.toggle('active');
 }
 
 items.forEach(item => item.addEventListener('click', toggleAccordion));
-
