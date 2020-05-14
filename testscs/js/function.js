545cfaa38de3268f33d340bdcdc0d9b402cd8106
Jordan Christensen <Jordan@Jordans-Mac-mini.local>
2020-05-14 13:50:56 -0600

    Updated survey functionality.

--- testscs/js/function.js
+++ testscs/js/function.js
@@ -42,8 +42,8 @@ window.onclick = function(event) {
   }
 }
 
-/* FIND A WAY TO MAKE UNINITIALIZED PROPERTIES??? */
-var Survey = {
+/* MODAL FUNCTIONALITY-----------------------------------------------------------------------*/
+var survey = {
   questions: [
     'HOW OFTEN DO YOU FEEL HOPELESS?',
     'QUESTION 2',
@@ -57,63 +57,171 @@ var Survey = {
     'QUESTION 10'
   ],
   questionSelections: [
-
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null
   ],
   currentQuestion: 0,
 };
 
 var nextQuestionBtn = document.getElementById("next");
 var backQuestionBtn = document.getElementById("back");
-var questions = [
-  'HOW OFTEN DO YOU FEEL HOPELESS?',
-  'QUESTION 2',
-  'QUESTION 3',
-  'QUESTION 4',
-  'QUESTION 5',
-  'QUESTION 6',
-  'QUESTION 7',
-  'QUESTION 8',
-  'QUESTION 9',
-  'QUESTION 10'
-];
-var currentQuestion = 0;
+var depressionQuestion1 = document.getElementById("depression-question1_0");
+var depressionQuestion2 = document.getElementById("depression-question1_1");
+var depressionQuestion3 = document.getElementById("depression-question1_2");
+var depressionQuestion4 = document.getElementById("depression-question1_3");
+var depressionQuestion5 = document.getElementById("depression-question1_4");
+
+depressionQuestion1.onclick = function(event) {
+  // Set the selected option.
+  survey.questionSelections[survey.currentQuestion] = "Never";
+
+  // Unselect all other checkboxes.
+  depressionQuestion2.checked = false;
+  depressionQuestion3.checked = false;
+  depressionQuestion4.checked = false;
+  depressionQuestion5.checked = false;
+}
+
+depressionQuestion2.onclick = function(event) {
+  // Set the selected option.
+  survey.questionSelections[survey.currentQuestion] = "Rarely";
+
+  // Unselect all other checkboxes.
+  depressionQuestion1.checked = false;
+  depressionQuestion3.checked = false;
+  depressionQuestion4.checked = false;
+  depressionQuestion5.checked = false;
+}
+
+depressionQuestion3.onclick = function(event) {
+  // Set the selected option.
+  survey.questionSelections[survey.currentQuestion] = "Sometimes";
+
+  // Unselect all other checkboxes.
+  depressionQuestion1.checked = false;
+  depressionQuestion2.checked = false;
+  depressionQuestion4.checked = false;
+  depressionQuestion5.checked = false;
+}
+
+depressionQuestion4.onclick = function(event) {
+  // Set the selected option.
+  survey.questionSelections[survey.currentQuestion] = "Often";
+
+  // Unselect all other checkboxes.
+  depressionQuestion1.checked = false;
+  depressionQuestion2.checked = false;
+  depressionQuestion3.checked = false;
+  depressionQuestion5.checked = false;
+}
+
+depressionQuestion5.onclick = function(event) {
+  // Set the selected option.
+  survey.questionSelections[survey.currentQuestion] = "Always";
+
+  // Unselect all other checkboxes.
+  depressionQuestion1.checked = false;
+  depressionQuestion2.checked = false;
+  depressionQuestion3.checked = false;
+  depressionQuestion4.checked = false;
+}
 
 nextQuestionBtn.onclick = function(event) {
-  // Create an alert!
-  //alert("Entering next question!");
+  if (survey.currentQuestion + 1 < survey.questions.length){
+    // Create an alert!
+    //alert("Entering next question!");
 
-  // Get necessary elements.
-  var questionText = document.getElementById('questionText');
-  var questionNumber = document.getElementById('question-number');
+    // Get the selected item.
+    //alert(survey.questionSelections[survey.currentQuestion]);
 
-  // Update the currentQuestion index.
-  currentQuestion++;
+    // Get necessary elements.
+    var questionText = document.getElementById('questionText');
+    var questionNumber = document.getElementById('question-number');
 
-  // Update the question.
-  questionText.innerText = questions[currentQuestion];
+    // Update the currentQuestion index.
+    survey.currentQuestion++;
 
-  // Update the question number.
-  questionNumber.innerText = "Question " + (currentQuestion + 1) + " of 10";
+    // Update the question.
+    questionText.innerText = survey.questions[survey.currentQuestion];
 
-  // Update the bullet points (if necessary).
+    // Update the question number.
+    questionNumber.innerText = "Question " + (survey.currentQuestion + 1) + " of 10";
+
+    // Reset the checkboxes.
+    depressionQuestion1.checked = false;
+    depressionQuestion2.checked = false;
+    depressionQuestion3.checked = false;
+    depressionQuestion4.checked = false;
+    depressionQuestion5.checked = false;
+
+    if (survey.questionSelections[survey.currentQuestion] != null){
+      // Restore the selected checkbox if possible.
+      if (survey.questionSelections[survey.currentQuestion] == "Never"){
+        depressionQuestion1.checked = true;
+      } else if (survey.questionSelections[survey.currentQuestion] == "Rarely"){
+        depressionQuestion2.checked = true;
+      } else if (survey.questionSelections[survey.currentQuestion] == "Sometimes"){
+        depressionQuestion3.checked = true;
+      } else if (survey.questionSelections[survey.currentQuestion] == "Often"){
+        depressionQuestion4.checked = true;
+      } else if (survey.questionSelections[survey.currentQuestion] == "Always"){
+        depressionQuestion5.checked = true;
+      }
+    }
+  } else {
+    // END OF SURVEY FUNCTIONALITY
+  }
 }
 
 backQuestionBtn.onclick = function(event) {
-  // Create an alert!
-  //alert("Entering previous question!");
+  if (survey.currentQuestion > 0){
+    // Create an alert!
+    //alert("Entering previous question!");
 
-  // Get necessary elements.
-  var questionText = document.getElementById("questionText");
-  var questionNumber = document.getElementById("question-number");
+    // Get necessary elements.
+    var questionText = document.getElementById("questionText");
+    var questionNumber = document.getElementById("question-number");
 
-  // Update the currentQuestion index.
-  currentQuestion--;
+    // Update the currentQuestion index.
+    survey.currentQuestion--;
 
-  // Update the question.
-  questionText.innerText = questions[currentQuestion];
+    // Check the current item.
+    //alert(survey.questionSelections[survey.currentQuestion]);
 
-  // Update the question number.
-  questionNumber.innerText = "Question " + (currentQuestion + 1) + " of 10";
+    // Update the question.
+    questionText.innerText = survey.questions[survey.currentQuestion];
 
-  // Update the bullet points (if necessary).
+    // Update the question number.
+    questionNumber.innerText = "Question " + (survey.currentQuestion + 1) + " of 10";
+
+    // Reset the checkboxes.
+    depressionQuestion1.checked = false;
+    depressionQuestion2.checked = false;
+    depressionQuestion3.checked = false;
+    depressionQuestion4.checked = false;
+    depressionQuestion5.checked = false;
+
+    if (survey.questionSelections[survey.currentQuestion] != null){
+      // Restore the selected checkbox if possible.
+      if (survey.questionSelections[survey.currentQuestion] == "Never"){
+        depressionQuestion1.checked = true;
+      } else if (survey.questionSelections[survey.currentQuestion] == "Rarely"){
+        depressionQuestion2.checked = true;
+      } else if (survey.questionSelections[survey.currentQuestion] == "Sometimes"){
+        depressionQuestion3.checked = true;
+      } else if (survey.questionSelections[survey.currentQuestion] == "Often"){
+        depressionQuestion4.checked = true;
+      } else if (survey.questionSelections[survey.currentQuestion] == "Always"){
+        depressionQuestion5.checked = true;
+      }
+    }
+  }
 }
