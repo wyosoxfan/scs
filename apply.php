ec47952f1b6e0e37417541c30287b5954107f976
neiljchristensen@outlook.com <neiljchristensen@outlook.com>
2020-08-16 13:19:18 -0600

    vcv

--- apply.php
+++ apply.php
@@ -1,20 +1,22 @@
 <?php
 
+ini_set('display_errors', 1);
+include_once('couchdb\Couch.php');
+include_once('couchdb\CouchClient.php');
+include_once('couchdb\CouchDocument.php');
+use  PHPOnCouch\CouchClient; //The CouchDB client object
+use  PHPOnCouch\CouchDocument; //The CouchDocument object
+
+// Set a new connector to the CouchDB server
+$client = new CouchClient('http://admin:Pu758KazLT@localhost:5984', 'employment');
+
 // Show all URL parameters (and
 // all form data submitted via the 
 // 'get' method)
-foreach($_POST as $key=>$value){
-    echo $key, ' => ', $value, "<br/>n";
+$doc = new CouchDocument($client);
+//foreach($_POST as $key=>$value){
+//	doc->{$key} = $value;
+    //echo $key, ' => ', $value, "<br/>n";
 }
 
-// Show a particular value.
-$id = $_POST['id'];
-
-if($id) {
-    echo '<p/>ID: ', $id, "<br/>n";
-}
-else {
-    echo '<p>No ID parameter.</p>';
-}
-
-?>
+?> 
