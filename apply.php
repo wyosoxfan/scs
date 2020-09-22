922d67becedd3be32e307816d719de03eed32d88
Neil Christensen <wyosoxfan@outlook.com>
2020-09-22 08:40:00 -0600

    Employment application work

--- apply.php
+++ apply.php
@@ -7,16 +7,54 @@ include_once('couchdb\CouchDocument.php');
 use  PHPOnCouch\CouchClient; //The CouchDB client object
 use  PHPOnCouch\CouchDocument; //The CouchDocument object
 
-// Set a new connector to the CouchDB server
-$client = new CouchClient('http://admin:Pu758KazLT@localhost:5984', 'employment');
 
-// Show all URL parameters (and
-// all form data submitted via the 
-// 'get' method)
-$doc = new CouchDocument($client);
-//foreach($_POST as $key=>$value){
-//	doc->{$key} = $value;
-    //echo $key, ' => ', $value, "<br/>n";
-//}
+//Entry point for the rest api
+$possible_get_actions = array("list");
+$possible_post_actions = array("apply");
+if(isset($_GET["action"]) && in_array($_GET["action"], $possible_get_actions))
+{
+	switch($_GET["action"])
+	{
+		case "list":
+			$application_list = array();
+			
+			$application1->name = 'Neil Christensen';
+			$application1->job = "Job #1";
+			
+			array_push($application_list, $application1);
+			
+			return $application_list;
+	}
+}
+else if(isset($_POST["action"]) && in_array($POST["action"], $possible_post_actions))
+{
+	switch($_POST["action"])
+	{
+		case "apply":
+			save_application($_POST);
+			break;
+	}
+}
+
+function save_application($post)
+{
+	// Set a new connector to the CouchDB server
+	$client = new CouchClient('http://admin:Pu758KazLT@localhost:5984', 'employment');
+
+	//Create the document
+	$doc = new CouchDocument($client);
+	
+	//Save the fields
+	foreach($_POST as $key=>$value){
+		doc->{$key} = $value;
+	}
+}
+
+function get_application_list()
+{
+	
+}
+
+
 
 ?> 
