976887f82a25cf9f38a0e3d2c2f2d785967ad287
Jordan Christensen <Jordan@Jordans-Mac-mini.local>
2020-09-23 17:26:07 -0600

    Updated database loader.

--- js/updateDB.js
+++ js/updateDB.js
@@ -1,14 +1,41 @@
 function updateDB() {
-    // Get all of the pages in the directory...
-    // For each page...
-    // Get the page name.
-    // Get the page URL.
-    // Create a row for the page...
-    // Get the elements on the page.
-    // For each element...
-    // Get the element id.
-    // Get the html for the elements.
-    // Create a row for the element...
-    // Get the inner text if it exists.
-    // Create a row for the content...
+    var xhttp;                                                // Connect to the database.
+    xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            alert(this.responseText);
+            alert("Database has been updated!"); // Send a message saying that the database has been updated.
+	    }
+    };
+
+    var pageTitle = document.title;                           // Get the name of the page.
+    var pageURL = window.location.href;                       // Get the URL of the page.
+    var pageElements = getElementsByTagName('*');             // Get all elements on the page.
+    var elements = [];                                        // List of elements to be sent to the database.
+
+    function Element(id, html, text, pageID) {
+        this.id = id;
+        this.html = html;
+        this.text = text;
+        this.pageID = pageID;
+    }
+
+    var elementID;
+    var elementHTML;
+    var elementText;
+    for (var i = 0; i < pageElements.length(); i++) {
+        elementID = pageElements[i].id;                       // Get the id.
+	    elementHTML = pageElements[i].outerHTML;              // Get the html.
+	    elementText = pageElements[i].innerText;              // Get the text.
+        element = new Element(elementID, elementHTML, 
+            elementText, pageTitle);                          // Create a new object.
+	    elements.push(element);                               // Store into the array.
+    }
+
+    $.post("./searchbardb/updateDB.php", 
+    { 
+	    qPageTitle: pageTitle,
+	    qPageURL: pageURL,
+	    qElements: elements
+    });
 }
