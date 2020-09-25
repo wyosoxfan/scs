40ecd6e23feb6ca67c0034c1f679c859bd15d934
Jordan Christensen <Jordan@Jordans-Mac-mini.local>
2020-09-25 15:12:08 -0600

    Database loader updated.

--- searchbardb/updateDB.php
+++ searchbardb/updateDB.php
@@ -8,7 +8,10 @@ try {
         $pageTitle = $_REQUEST["qPageTitle"];                                                    // Get the page title.
         $pageURL = $_REQUEST["qPageURL"];                                                        // Get the page URL.
         $elements = $_REQUEST["qElements"];                                                      // Get the elements on the page.
-        echo var_dump($elements[0]);
+        $elementArrayItem1 = json_decode($elements[0]);
+        //echo var_dump($elementArrayItem1);
+        //echo "Individual element property:";
+        //echo var_dump($elementArrayItem1->id);
 
         $pageStmt = $pdo->prepare("INSERT INTO page VALUES (:name, :URL)");                      // Create the insert query for the page data.
         $elementStmt = $pdo->prepare("INSERT INTO element VALUES (:id, :html, :text, :pageID)"); // Create the insert query for the element data.
@@ -16,11 +19,13 @@ try {
         $pageStmt->bindParam(':URL', $pageURL);
         $pageStmt->execute();                                                                    // Update the database.
 
-        for ($x = 0; $x <= 10; $x++) {                                                           // Loop through all of the elements and create insert queries...
-            $elementID; // Get the id of the element.
-            $elementHTML; // Get the html of the element.
-            $elementText; // Get the text of the element.
-            $elementPageID; // Get the pageID of the element.
+        for ($i = 0; $i <= count($elements); $i++) {                                             // Loop through all of the elements and create insert queries...
+            echo "Element " . $i . "\n";
+            $elementObject = json_decode($elements[$i]);                                         // Get the element.
+            $elementID = $elementObject->id;                                                     // Get the id of the element.
+            $elementHTML = $elementObject->html;                                                 // Get the html of the element.
+            $elementText = $elementObject->text;                                                 // Get the text of the element.
+            $elementPageID = $elementObject->pageID;                                             // Get the pageID of the element.
             $elementStmt->bindParam(':id', $elementID);                                          // Prepare the sql statement.
             $elementStmt->bindParam(':html', $elementHTML);
             $elementStmt->bindParam(':text', $elementText);
